fd40927ea92768d23ad1e8c427a4a5e41e3d6a77
JN Zhang <zhangjianan1229@163.com>
2020-08-31 18:45:31 +0800

    zjn

--- cloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
@@ -2,16 +2,14 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableConfigServer
 @EnableEurekaClient
-public class ConfigCenterMain3344 {
+public class ConfigClientMain3355 {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigCenterMain3344.class, args);
+        SpringApplication.run(ConfigClientMain3355.class, args);
     }
 
 }
